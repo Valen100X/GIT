7dd4f7bc548610c09947b8f6c7345d2161952ba7
Valen100X <vscotti@lasalleflorida.edu.ar>
2024-05-13 16:02:13 -0300

    add

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path'); // Import path module
+const { error } = require('console');
+
+const app = express();
+const port = 3001;
+
+// Allow all cross-origin requests
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
